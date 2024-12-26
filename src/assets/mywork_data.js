e79cc89c7e11b3c1923b9f138c829511a0038d2a
MarcosCrRodriguez <programmer.crod@gmail.com>
2024-12-26 19:02:13 -0300

    new-projects

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -4,6 +4,8 @@ import project3_img from "../assets/clinica.png";
 import project5_img from "../assets/f1-web.png";
 import project6_img from "../assets/app-angular.png";
 import project4_img from "../assets/tablon-peliculas.png";
+import project7_img from "../assets/img-perfy.png";
+import project8_img from "../assets/arcane-web2.png";
 
 const mywork_data = [
   {
@@ -25,6 +27,12 @@ const mywork_data = [
     w_link: "https://simulacro-app.web.app/peliculas",
   },
   {
+    w_no: 7,
+    w_name: "Web design",
+    w_img: project7_img,
+    w_link: "https://perfy-drinks.netlify.app/",
+  },
+  {
     w_no: 5,
     w_name: "Web design",
     w_img: project6_img,
@@ -41,6 +49,12 @@ const mywork_data = [
     w_name: "Web design",
     w_img: project5_img,
     w_link: "https://formulaone-angular.netlify.app/",
+  },
+  {
+    w_no: 8,
+    w_name: "Web design",
+    w_img: project8_img,
+    w_link: "https://angular-arcanefanpage.netlify.app/presentacion",
   },
 ];
 
